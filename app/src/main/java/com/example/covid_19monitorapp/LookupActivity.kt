b6c0e5367a942a60a4d387ffab5d85b6574a2727
Final9800 <cunardyvincent12@gmail.com>
2020-09-07 23:16:54 +0700

    redirect and pass intent data

--- app/src/main/java/com/example/covid_19monitorapp/LookupActivity.kt
+++ app/src/main/java/com/example/covid_19monitorapp/LookupActivity.kt
@@ -2,6 +2,8 @@ package com.example.covid_19monitorapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_lookup.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,21 +11,48 @@ import kotlinx.android.synthetic.main.activity_main.lookUpArrIcon
 
 class LookupActivity: AppCompatActivity() {
     companion object{
-        const val Extra="Extra"
+        const val Extra="Extras"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lookup)
+        title= "Lookup Activity"
 
-        backButton.setOnClickListener(){
-            homeActivity()
-        }
+        val value = intent.getStringExtra(Extra)
+        search.setQuery(value,false)
 
-    }
-    private fun homeActivity(){
-        val intent = Intent(this, MainActivity::class.java).apply{
-            putExtra(Extra,"Halo")
+        backButton.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
         }
-        startActivity(intent)
     }
+    override fun onStart() {
+        super.onStart()
+        Log.e("Activity","Activity:onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("Activity","Activity:onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("Activity","Activity:onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("Activity","Activity:onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("Activity","Activity:onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("Activity","Activity:onDestroy")
+    }
+
 }
