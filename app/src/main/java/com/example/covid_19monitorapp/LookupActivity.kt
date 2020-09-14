1216c6b03508a46d6aed83a93144c50466ceacc3
Herman <huangherman41@gmail.com>
2020-09-15 01:26:32 +0700

    test creating search function using search view

--- app/src/main/java/com/example/covid_19monitorapp/LookupActivity.kt
+++ app/src/main/java/com/example/covid_19monitorapp/LookupActivity.kt
@@ -4,22 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.covid_19monitorapp.Adapter.LookUpAdapter
 import kotlinx.android.synthetic.main.activity_lookup.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.lookUpArrIcon
+import java.util.*
 
 class LookupActivity: AppCompatActivity() {
+
+
     companion object{
         const val Extra="Extras"
         private val mockLookUpList = mutableListOf(
-            LookUpData("DKI Jakarta",800,500,300),
-            LookUpData("Sumatera Utara",120,80,30),
-            LookUpData("Sumatera Selatan",50,60,85),
-            LookUpData("Sumatera Barat",959,26,5),
-
             LookUpData("DKI Jakarta",800,500,300),
             LookUpData("Sumatera Utara",120,80,30),
             LookUpData("Sumatera Selatan",50,60,85),
@@ -33,14 +32,34 @@ class LookupActivity: AppCompatActivity() {
         title= "Lookup Activity"
 
         val value = intent.getStringExtra(Extra)
-        search.setQuery(value,false)
+//        svProvinceSearch.setQuery(value,false)
 
         backButton.setOnClickListener{
             onBackPressed()
         }
 
+        svProvinceSearch.setOnQueryTextListener(searchOnTextChange(mockLookUpList))
+
         rvLookUp.layoutManager =LinearLayoutManager(this)
         rvLookUp.adapter = lookUpAdapter
+    }
+
+
+    private fun searchOnTextChange(listOfLookUpData : MutableList<LookUpData>) : SearchView.OnQueryTextListener {
+        return object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                filterLookUpData(listOfLookUpData, p0)
+                return true;
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false;
+            }
+        }
+    }
+
+    private fun filterLookUpData(listOfLookUpData: MutableList<LookUpData>, keyword : String?) {
+
     }
 
     override fun onStart() {
