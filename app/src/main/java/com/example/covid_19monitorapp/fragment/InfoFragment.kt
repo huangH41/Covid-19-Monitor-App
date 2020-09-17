852c6c1672e19a66c9a714d2b183196ef970bdf7
Gerald Stephanus <geraldstephanus@gmail.com>
2020-09-17 12:52:07 +0700

    Small visual fix, more info added

--- app/src/main/java/com/example/covid_19monitorapp/fragment/InfoFragment.kt
+++ app/src/main/java/com/example/covid_19monitorapp/fragment/InfoFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.covid_19monitorapp.R
 import kotlinx.android.synthetic.main.dialog_info.*
@@ -20,6 +21,13 @@ class InfoFragment: DialogFragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var counter: Int = 10
+
+        hints.setOnClickListener(){
+            if(counter > 1) counter--
+            else Toast.makeText(activity, "Have a good day!", Toast.LENGTH_LONG).show()
+        }
 
         closeInfo.setOnClickListener(){
             dismiss()
